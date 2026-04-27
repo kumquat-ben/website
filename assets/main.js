3a7563e70a0ca10d76ca555564591263289aab2e
kumquat <armenmerik@gmail.com>
2026-04-27 15:13:43 -0700

    Build static Kumquat lead list site

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+const toggle = document.querySelector(".nav-toggle");
+const menu = document.querySelector("#nav-menu");
+
+if (toggle && menu) {
+  toggle.addEventListener("click", () => {
+    const isOpen = menu.classList.toggle("is-open");
+    toggle.setAttribute("aria-expanded", String(isOpen));
+  });
+}
